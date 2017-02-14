abe295cc8dbca505d468374260838df4006129a9
tkwatanabe <tkwatanabe@shinwart.co.jp>
2017-02-14 19:32:34 +0900

    単一選択リスト

--- app/src/main/java/com/example/tkwatanabe/webview_sample/ListViewActivity.java
+++ app/src/main/java/com/example/tkwatanabe/webview_sample/ListViewActivity.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -34,7 +35,8 @@ public class ListViewActivity extends AppCompatActivity {
         data.add("ggg");
 
         //配列アダプターを作成&ListViewに登録
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, data); //単一選択可能なリスト
         ListView list = (ListView)findViewById(R.id.lst_view);
         list.setAdapter(adapter);
 
@@ -48,18 +50,30 @@ public class ListViewActivity extends AppCompatActivity {
 //                }
 //        );
         //リスト項目を長押しした時の処理を定義
-        list.setOnItemLongClickListener(
-                new AdapterView.OnItemLongClickListener() {
-                    public boolean onItemLongClick(
-                            AdapterView<?> av,
-                            View view,
-                            int position,
-                            long id) {
-                        adapter.remove((String)((TextView)view).getText());
-                        return false;
+//        list.setOnItemLongClickListener(
+//                new AdapterView.OnItemLongClickListener() {
+//                    public boolean onItemLongClick(
+//                            AdapterView<?> av, //選択されたリストの親となるウィジット。ここではListView
+//                            View view, //選択された項目
+//                            int position, //選択された項目の位置
+//                            long id) { //選択された項目のidの値
+//                        adapter.remove((String)((TextView)view).getText());
+//                        return false;
+//                    }
+//                }
+//        );
+        //リスト項目をクリックした時の処理を定義(単一選択可能)
+        list.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> av, View view, int position, long id) {
+                        CharSequence msg = ((TextView) view).getText();
+                        Toast.makeText(ListViewActivity.this,
+                                String.format("選択したのは%s", msg.toString()),
+                                Toast.LENGTH_SHORT).show();
                     }
                 }
         );
+
     }
 
 }
