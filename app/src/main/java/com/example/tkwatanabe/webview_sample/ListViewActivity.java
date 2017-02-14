8d88a9102db22861a1b96aac7603153f0d54e39c
渡邊丈洋 <mogu.224@gmail.com>
2017-02-14 23:27:19 +0900

    list更新

--- app/src/main/java/com/example/tkwatanabe/webview_sample/ListViewActivity.java
+++ app/src/main/java/com/example/tkwatanabe/webview_sample/ListViewActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ import java.util.ArrayList;
 public class ListViewActivity extends AppCompatActivity {
 
     ArrayAdapter<String> adapter;
+    ListView list;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,8 +38,9 @@ public class ListViewActivity extends AppCompatActivity {
 
         //配列アダプターを作成&ListViewに登録
 //        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, data); //単一選択可能なリスト
-        ListView list = (ListView)findViewById(R.id.lst_view);
+//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, data); //単一選択可能なリスト
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, data); //複数選択可能なリスト、リスト項目にチェックボックスを追加するためのレイアウト
+        list = (ListView)findViewById(R.id.lst_view);
         list.setAdapter(adapter);
 
         //リスト項目をクリックした時の処理を定義
@@ -63,17 +66,32 @@ public class ListViewActivity extends AppCompatActivity {
 //                }
 //        );
         //リスト項目をクリックした時の処理を定義(単一選択可能)
+//        list.setOnItemClickListener(
+//                new AdapterView.OnItemClickListener() {
+//                    public void onItemClick(AdapterView<?> av, View view, int position, long id) {
+//                        CharSequence msg = ((TextView) view).getText();
+//                        Toast.makeText(ListViewActivity.this,
+//                                String.format("選択したのは%s", msg.toString()),
+//                                Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//        );
+        //リスト項目をクリックした時の処理を定義(複数選択可能) android:choiceMode="multipleChoice"
         list.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     public void onItemClick(AdapterView<?> av, View view, int position, long id) {
-                        CharSequence msg = ((TextView) view).getText();
-                        Toast.makeText(ListViewActivity.this,
-                                String.format("選択したのは%s", msg.toString()),
-                                Toast.LENGTH_SHORT).show();
+                        String msg = "選択したのは、";
+                        for(int i = 0; i < list.getChildCount(); i++) { //ListView配下の項目数を取得「getChildCount」
+                            //複数選択可能なリストはCheckedTextViewとして表される。
+                            CheckedTextView check = (CheckedTextView) list.getChildAt(i); //ListView配下の指定の項目を取得する「getChildAt」
+                            if(check.isChecked()) {
+                                msg += check.getText() + ","; //getText()で項目テキストを取得できる。
+                            }
+                        }
+                        msg = msg.substring(0, msg.length() - 1);
+                        Toast.makeText(ListViewActivity.this, msg, Toast.LENGTH_SHORT).show();
                     }
                 }
         );
-
     }
-
 }
